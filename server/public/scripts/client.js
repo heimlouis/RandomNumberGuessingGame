5fb9c703933f9bb145c80808a6281999a6ae95d1
Jacob Albright <jacob.albright23@gmail.com>
2018-02-09 10:20:36 -0600

    starting to write the JS to hide and show html for setup mode and gameplay mode

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,37 +1,51 @@
 $(document).ready(onReady)
 
-function onReady(){
+function onReady() {
     console.log('Jacob is great!');
+    setupMode();
+    $('#STARTGAME').on('click'){
+        // hide the html from setupMode
+        // run gamePlay
+    }
+
+}
+
+// setupMode() handles all the client side JS 
+function setupMode() {
+    // when we click #btn-max, call addNumber
     $('#btn-max').on('click', function () {
-    addNumber ();
-    })
+        addNumber();
+    });
 
+    // 
+    function addNumber() {
+        // store an object with the maximum value possible for the random number in the variable newMax
+        let newMax = getRangeMax();
+        // use ajax to send newMax 
+        $.ajax({
+            type: 'POST',
+            url: '/game/add',
+            data: { number: newMax }
+        }).done(function (response) {
+            console.log('added');
+            //we may need to perform other functions but not yet
+            clearInputForm();
+        }).fail(function (error) {
+            console.log(error);
+        })
+    }
+
+
+    function getRangeMax() {
+        let maxRange = $('#numberGuessMax').val();
+        let rangeObject = {
+            max: maxRange
+        };
+        return rangeObject;
+    }
+
+    function clearInputForm() {
+        $('#numberGuessMax').val('');
+    }
 }
 
-function addNumber () {
-    let newMax = getRangeMax ();
-    $.ajax ({
-        type:'POST',
-        url: '/game/add',
-        data: {number: newMax}
-    }).done(function(response){
-        console.log('added');
-        //we may need to perform other functions but not yet
-        clearInputForm();
-    }).fail(function (error){
-        console.log(error);
-    })
-}
-
-
-function getRangeMax () {
-    let maxRange = $('#numberGuessMax').val(); 
-    let rangeObject = {
-        max: maxRange
-    };
-    return rangeObject;
-}
-
-function clearInputForm() {
-    $('#numberGuessMax').val('');
-}
